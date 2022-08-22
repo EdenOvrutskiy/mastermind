73b2fe56138dd7d7ca93b12ccc4c8a30f81b417b
Eden O <-->
2022-08-22 09:47:23 -0400

    added Game class that uses Feedback to play a game

--- mastermind.rb
+++ mastermind.rb
@@ -4,8 +4,8 @@
 
 class Guess
   #gets a guess from the user
-  attr_reader :length, :guess
   private
+  attr_reader :length
   def initialize
     @length = 4
     @guess = get_guess
@@ -13,14 +13,42 @@ class Guess
 
   def get_color
     begin
-      puts "enter a color: "
+      def prompt_for_color
+        puts "select a color by entering its' corresponding digit:"
+        puts "1 -> blue"
+        puts "2 -> red"
+        puts "3 -> yellow"
+        puts "4 -> orange"
+        puts "5 -> green"
+        puts "6 -> purple"
+        puts "enter a digit: "
+      end
+      prompt_for_color
       input = gets
-      color = input.chomp.to_sym
+      digit = input.chomp.to_i
+      color = number_to_color(digit)
+      puts "you have selected: #{color}"
+      color
     rescue
       puts "bad input, try again"
     end
   end
-  
+
+  def number_to_color(number)
+    case number
+    when 1 then :blue
+    when 2 then :red
+    when 3 then :yellow
+    when 4 then :orange
+    when 5 then :green
+    when 6 then :purple
+    else
+      nil
+    end
+  end
+
+  public
+  attr_reader :guess
   def get_guess
     puts "Input a guess (4 inputs, 1 per color):"
     guess = []
@@ -32,12 +60,14 @@ class Guess
   end
 end
 
+
+
 class Feedback
   #provides feedback for a user's guess
   private
   attr_reader :guess, :solution
-  def initialize(guess)
-    @solution = [:green, :blue, :red, :yellow]
+  def initialize(guess,solution)
+    @solution = solution
     @guess = guess
   end
 
@@ -46,6 +76,7 @@ class Feedback
       guess == solution
     end
   end
+
   
   def create_color_pair(index)
     #creates a pair of colors from a guess+solution 
@@ -75,10 +106,79 @@ class Feedback
   
   def count_correct_colors_at_wrong_positions
     (count_correct_colors - count_correct_positions).abs
-  end  
+  end
+
+  def display
+    puts "You've guessed: #{count_correct_positions} " +
+         "correct positions, " + 
+         "and #{count_correct_colors_at_wrong_positions} " + 
+         "colors in wrong positions"
+  end
+
+  def all_correct?
+    count_correct_positions == solution.length
+  end
+
+  def view_solution
+    puts solution
+  end
 end
 
 
+class Game
+  private
+  attr_reader :current_turn, :feedback, :turn_limit, :solution
+  attr_writer :current_turn
+  def initialize(solution)
+    @turn_limit = 12
+    @current_turn = 0
+    @solution = solution
+  end
 
-#Feedback.new([:green, :blue, :yellow, :red]).count_correct_positions
+  def display_current_turn
+    puts "turn #{current_turn}"
+  end
+
+  def increment_turn
+    self.current_turn += 1
+  end
+
+  def process_guess
+    @feedback = Feedback.new(Guess.new.guess, solution)
+    feedback.display
+  end
+
+  def game_over?
+    (feedback.all_correct?) || (current_turn == turn_limit)
+  end
+
+  public  
+  def play
+    loop do
+      increment_turn
+      display_current_turn
+      process_guess
+      break if game_over?
+    end
+  end
+end
+
+def random_color
+  [:blue, :red, :yellow, :orange, :green, :purple].sample
+end
+
+def generate_solution
+  array = Array.new
+  while array.length < 4 do
+    color = random_color
+    unless array.include?(color) 
+      array.push(color)
+    end
+  end
+  array
+end
+
+solution = generate_solution
+  
+Game.new(solution).play
 
