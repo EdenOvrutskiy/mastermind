3e65cfca5ee1987a34db44fb009c24851d29ac1a
Eden O <-->
2022-08-23 19:57:15 -0400

    Create Color_sequence class to DRY the guess length and color pallete, replaced Pair Struct + related method with .zip array method

--- mastermind.rb
+++ mastermind.rb
@@ -2,22 +2,61 @@
 #no duplicate colors (in guess or solution)
 #no blank colors
 
-class Guess
-  #gets a guess from the user
+class Color_sequence
+  #Guess and Solution's shared template.
+  attr_reader :sequence_length
+  def initialize
+    @sequence_length = 4
+    @colors = [:blue, :red, :yellow, :orange, :green, :purple]
+  end
+
+  def guess
+    Guess.new.get_guess
+  end
+
+  def solution
+    Solution.new.generate
+  end
+end
+
+class Solution < Color_sequence
+  private
+  attr_reader :colors
+  
+  def random_color
+    colors.sample
+  end
+  
   public
-  attr_reader :guess
+  def generate
+    array = Array.new
+    while array.length < sequence_length do
+      unless array.include?(random_color) 
+        array.push(random_color)
+      end
+    end
+    array
+  end
+end
+
+class Guess < Color_sequence
+  #gets a guess from the user
+  attr_reader :colors
+  public
+  def get_guess
+    display_digit_to_color_mapping
+    prompt_for_guess
+    get_valid_input
+    input_into_guess
+  end
   
   private
-  attr_reader :length, :input
-  def initialize
-    @length = 4
-    @guess = get_guess
-  end
+  attr_reader :input
 
   def valid_input?
     chomped_input = input.chomp
     chomped_input.is_a?(String) &&
-      chomped_input.length == length &&
+      chomped_input.length == sequence_length &&
       is_number(chomped_input)
   end
   
@@ -40,16 +79,7 @@ class Guess
   end
   
   def digit_to_color(digit)
-    case digit
-    when 1 then :blue
-    when 2 then :red
-    when 3 then :yellow
-    when 4 then :orange
-    when 5 then :green
-    when 6 then :purple
-    else
-      nil
-    end
+    colors[digit - 1]
   end
 
   def strings_to_digits(strings)
@@ -61,12 +91,12 @@ class Guess
   def digits_to_colors(digits)
     #[1, 2, 3] -> [:blue, :red, :yellow]
     colors = digits.map{|digit| digit_to_color(digit)}
-    
   end
 
   def input_into_guess
     # "1234" -> [:blue, :red, :yellow, :orange]
-    strings = input.split('')
+    chomped_input = input.chomp
+    strings = chomped_input.split('')
     digits = strings_to_digits(strings)
     colors = digits_to_colors(digits)
   end
@@ -79,79 +109,49 @@ class Guess
       puts "bad input, try again"
     end
   end
-
-  def get_guess
-    display_digit_to_color_mapping
-    prompt_for_guess
-    get_valid_input
-    input_into_guess
-  end
 end
 
 class Feedback
   #provides feedback for a user's guess
   private
   attr_reader :guess, :solution
-  def initialize(guess,solution)
-    @solution = solution
+  def initialize(guess, solution) #expects 2 arrays
     @guess = guess
+    @solution = solution
   end
 
-  Color_pair = Struct.new(:guess, :solution) do
-    def same?
-      guess == solution
-    end
-  end
-
-  
-  def create_color_pair(index)
-    #creates a pair of colors from a guess+solution 
-    #that share the same position.
-    Color_pair.new(guess[index], solution[index]) 
-  end
-
-  def color_pairs
-    #returns an iterable with all color pairs
-    #(trusts that guess and solution have the same length)
-    Array.new(solution.length)
-      .map
-      .with_index {|redundant,i| create_color_pair(i)}
-  end
-  
   public
-  def count_correct_positions
-    #how many colors are correct + at the correct position?
-    color_pairs
-      .select{|pair| pair.same?}
-      .count
+  def correct_colors_at_correct_positions
+    pairs = guess.zip(solution)
+    pairs.select{|g, s| g == s}#2D array
+      .flatten
+      .uniq#1D array
   end
 
-  def count_correct_colors
-    guess.intersection(solution).count
+  def correct_colors_at_wrong_positions
+    shared_colors = guess.intersection(solution)
+    shared_colors - correct_colors_at_correct_positions
   end
+
+  #shared colors are either in the correct or incorrect position.
   
-  def count_correct_colors_at_wrong_positions
-    (count_correct_colors - count_correct_positions).abs
-  end
-
   def display
-    puts "You've guessed: #{count_correct_positions} " +
-         "correct positions, " + 
-         "and #{count_correct_colors_at_wrong_positions} " + 
-         "colors in wrong positions"
+    puts "You've guessed: " +
+         "#{correct_colors_at_wrong_positions.count} " +
+         "correct colors at the wrong position, " + 
+         "and #{correct_colors_at_correct_positions.count} " + 
+         "colors in correct positions"
   end
 
   def all_correct?
-    count_correct_positions == solution.length
-  end
-
-  def view_solution
-    puts solution
+    correct_colors_at_correct_positions.count == solution.length
   end
 end
 
-
 class Game
+  #creates a game :
+  # the users tries to guess a solution while
+  # getting feedback.
   private
   attr_reader :current_turn, :feedback, :turn_limit, :solution
   attr_writer :current_turn
@@ -170,12 +170,12 @@ class Game
   end
 
   def process_guess
-    @feedback = Feedback.new(Guess.new.guess, solution)
+    @feedback = Feedback.new(Color_sequence.new.guess, solution)
     feedback.display
   end
 
   def game_over?
-    (feedback.all_correct?) || (current_turn == turn_limit)
+    (feedback.all_correct?) || (current_turn >= turn_limit)
   end
 
   public  
@@ -189,22 +189,38 @@ class Game
   end
 end
 
-def random_color
-  [:blue, :red, :yellow, :orange, :green, :purple].sample
-end
+# class Display
+#   #knows what to display, displays it when called upon
+#   def digit_to_color_mapping
+#     puts "each digit corresponds to a color:"
+#     puts "1 -> blue"
+#     puts "2 -> red"
+#     puts "3 -> yellow"
+#     puts "4 -> orange"
+#     puts "5 -> green"
+#     puts "6 -> purple"
+#   end
 
-def generate_solution
-  array = Array.new
-  while array.length < 4 do
-    color = random_color
-    unless array.include?(color) 
-      array.push(color)
-    end
-  end
-  array
-end
+#   def guess_prompt
+#     puts "enter a guess (e.g '1234')"
+#   end
 
-solution = generate_solution
-  
-Game.new(solution).play
+#   def feedback
+#     puts "You've guessed: #{count_correct_positions} " +
+#          "correct positions, " + 
+#          "and #{count_correct_colors_at_wrong_positions} " + 
+#          "colors in wrong positions"
+#   end
+
+#   def bad_input_retry
+#     puts "bad input, try again"
+#   end
+
+#   def display_current_turn
+#     puts "turn #{current_turn}"
+#   end
+# end
+
+#Game.new(Color_sequence.new.solution).play
+Game.new([:blue, :red, :yellow, :orange]).play
 
